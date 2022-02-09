d6ca16f7460f196db95c7ce9bda2d7899d181891
Bruno Santos <bruno_cris_santos@hotmail.com>
2022-02-09 18:39:03 -0400

    feat(web): component list-pokemons

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
+import { ListPokemonComponent } from './pokemons/list-pokemon/list-pokemon.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, ComponentsModule],
+  declarations: [HomeComponent, ListPokemonComponent],
+  imports: [CommonModule, ComponentsModule, MatCardModule],
   exports: [HomeComponent],
 })
 export class ViewsModule {}
